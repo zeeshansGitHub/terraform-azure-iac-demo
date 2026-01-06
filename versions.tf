74a052f2f977271fd5b20d3f8e31caf2baeefb95
Zeeshan Abbas <67509023+zeeshansGitHub@users.noreply.github.com>
2026-01-06 14:23:35 -0600

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
